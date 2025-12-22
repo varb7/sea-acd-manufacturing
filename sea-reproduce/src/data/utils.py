e252175647758d4019ab46aa9c4b86e7832acb8f
varb7 <varun.bhoj@fau.de>
2025-12-22 16:30:24 +0100

    feat: Undirected edges compatibility

--- sea-reproduce/src/data/utils.py
+++ sea-reproduce/src/data/utils.py
@@ -79,9 +79,10 @@ except ImportError:
 edge_map_fci = {
     # 0 reserved for padding
     (2, 2): 1,  # (0, 0) no edge but not padded
+    (1, 1): 4,  # (-1, -1) undirected edge (after conversion from PyTetrad)
     (1, 3): 2,  # (-1, 1)
     (3, 1): 3,  # (1, -1)
-    (3, 3): 4,  # (1, 1)
+    (3, 3): 4,  # (1, 1) bidirected edge
     (3, 4): 5,  # (1, 2)
     (4, 3): 6,  # (2, 1)
     (4, 4): 7   # (2, 2)
